49edf4584f983e28508c96701f0195d642f64b69
Victor Varges <victorryandias@gmail.com>
2022-06-04 16:12:37 -0300

    config: import dotenv on connection

--- src/connection.ts
+++ src/connection.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
+dotenv.config();
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
