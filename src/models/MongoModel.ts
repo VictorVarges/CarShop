5716b4d830dafa402fbdb42d46c7729d402ddafa
Victor Varges <victorryandias@gmail.com>
2022-06-04 18:31:02 -0300

    feat: mongo model generics implemented

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,23 @@
+import { Model as MongooseModelInterface, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements Model<T> {
+  constructor(protected model: MongooseModelInterface<T & Document>) { }
+
+  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id: string): Promise<T | null> =>
+    this.model.findOne({ id });
+
+  async update(id: string, data: Partial<T>): Promise<T | null> {
+    return this.model.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return this.model.findByIdAndDelete(id);
+  }
+}
+
+export default MongoModel;
