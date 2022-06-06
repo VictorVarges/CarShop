d131787fee0e9e06d68354559ddf68fac56012c5
Victor Varges <victorryandias@gmail.com>
2022-06-06 01:44:06 -0300

    feat: route created

--- src/router/routs.ts
+++ src/router/routs.ts
@@ -16,6 +16,7 @@ class CustomRouter<T> {
     this.router.get(route, controller.read);
     this.router.get(`${route}/:id`, controller.readOne);
     this.router.put(`${route}/:id`, controller.up);
+    this.router.delete(`${route}/:id`, controller.delete);
   }
 }
 
