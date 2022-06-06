29bc29cb7ce5a7e9559d9316b66b4aeef208d5c0
Victor Varges <victorryandias@gmail.com>
2022-06-05 23:41:23 -0300

    feat: index implemented to create the connection with controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { MESSHTTP, CODEHTTP } from '../http/Responses';
 import Service from '../services/index';
 
 export type ResponseError = {
@@ -12,11 +13,28 @@ export interface RequestWithBody<T> extends Request {
 abstract class Controller<T> {
   abstract route: string;
 
+  protected mess = MESSHTTP;
+
+  protected code = CODEHTTP;
+
   constructor(protected service: Service<T>) { }
 
   abstract create(
     req: RequestWithBody<T>,
     res: Response<T | ResponseError>,
   ): Promise<typeof res>;
+
+  read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const objs = await this.service.read();
+      return res.json(objs);
+    } catch (err) {
+      return res.status(this.code.INTERNAL_CODE)
+        .json({ error: this.mess.INTERNAL });
+    }
+  };
 }
 export default Controller;
--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -12,7 +12,7 @@ const carSchema = new Schema<CarDocument>({
   buyValue: Number,
   doorsQty: Number,
   seatsQty: Number,
-}, { vesionKey: false });
+}, { versionKey: false });
 
 export default class CarModel extends MongoModel<Car> {
   constructor(model = createModel('Cars', carSchema)) {
